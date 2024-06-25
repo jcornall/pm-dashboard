018d0396b4575a03804fae5889186c0aad8f1a6e
jcornall <james.cornall@stfc.ac.uk>
2024-06-25 12:30:06 +0100

    Add logging module and /data/ directory check

--- __main__.py
+++ __main__.py
@@ -2,10 +2,15 @@ from util.vuln_export import *
 # from util.asset_export import *
 import logging
 import requests
+import os
 import json
-from types import SimpleNamespace
+import logging
+import datetime
 
-def generate_vulnexport():
+current_time = datetime.current_time()
+logging.basicConfig(filename=f"{current_time.strftime("%Y%m%d_%H%M%S")}.log", filemode="w", format="%(asctime)s:%(name)s:%(levelname)s - %(message)s")
+
+def request_vuln_export():
     url = "https://cloud.tenable.com/vulns/export"
     payload = {
         "num_assets": 50,
@@ -14,18 +19,18 @@ def generate_vulnexport():
     headers = {
         "accept": "application/json",
         "content-type": "application/json",
-        "X-ApiKeys": "accessKey=7becc28f63abced104af4af8c52f73cd21d3d36fd771172e03eaf4ceb241964f; secretKey=37f61b104ed8983d84f38e95d1470f07c620dfc4995263933fe2a43b49acf636;"
+        "X-ApiKeys": f"accessKey={os.environ["TENABLE_ACCESS_KEY"]}; secretKey={os.environ["TENABLE_SECRET_KEY"]};"
     }
     response = requests.post(url, json=payload, headers=headers)
     response_json = json.loads(response.text)
     export_uuid = response_json["export_uuid"]
     return 0, export_uuid
 
-def report_vulnexport_status(export_uuid):
+def request_vuln_export_status(export_uuid):
     url = f"https://cloud.tenable.com/vulns/export/{export_uuid}/status"
     headers = {
         "accept": "application/json",
-        "X-ApiKeys": "accessKey=7becc28f63abced104af4af8c52f73cd21d3d36fd771172e03eaf4ceb241964f; secretKey=37f61b104ed8983d84f38e95d1470f07c620dfc4995263933fe2a43b49acf636;"
+        "X-ApiKeys": f"accessKey={os.environ["TENABLE_ACCESS_KEY"]}; secretKey={os.environ["TENABLE_SECRET_KEY"]};"
     }
     response = requests.get(url, headers=headers)
     response_json = json.loads(response.text)
@@ -35,5 +40,11 @@ def report_vulnexport_status(export_uuid):
           f"\n - Total Chunks: {vuln_export.total_chunks}")
     return vuln_export
 
-test = report_vulnexport_status("70abb1f0-abd6-4cae-b42e-9eda2a0b8719")
-test.download_all_vuln_chunks()
+def check_data_folder():
+    print("Checking if /pmt-dashboard/data/ exists...")
+    if os.path.isdir("./data/"):
+        print("Directory exists.")
+    else:
+        print("Directory does not exist, creating directory...")
+        os.mkdir("data")
+        print("Directory created.")
--- util/vuln_export.py
+++ util/vuln_export.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import time
+import os
 
 class VulnExport:
 
@@ -17,27 +18,28 @@ class VulnExport:
         self.filters = response_json["filters"]
         self.num_assets_per_chunk = response_json["num_assets_per_chunk"]
         self.created = response_json["created"]
+        print(str(self))
 
     def download_all_vuln_chunks(self):
         for chunk in range(1, self.total_chunks):
-            self.vuln_download_chunk(chunk)
+            self.download_vuln_chunk(chunk)
         return 0
     
-    def vuln_download_chunk(self, chunk):
+    def download_vuln_chunk(self, chunk):
         print(f"Downloading chunk {chunk}")
         url = f"https://cloud.tenable.com/vulns/export/{self.uuid}/chunks/{chunk}"
         headers = {
             "accept": "application/octet-stream",
-            "X-ApiKeys": "accessKey=7becc28f63abced104af4af8c52f73cd21d3d36fd771172e03eaf4ceb241964f; secretKey=37f61b104ed8983d84f38e95d1470f07c620dfc4995263933fe2a43b49acf636"
+            "X-ApiKeys": f"accessKey={os.environ["TENABLE_ACCESS_KEY"]}; secretKey={os.environ["TENABLE_SECRET_KEY"]};"
         }
         response = requests.get(url, headers=headers)
         if response.status_code == 429:
-            print(f"Error Code 429, retrying...")
+            print(f"Error Code 429, retrying in {response.headers["Retry-After"]}...")
             time.sleep(int(response.headers["Retry-After"]))
-            self.vuln_download_chunk(chunk)
+            self.download_vuln_chunk(chunk)
         else:
             response_json = json.loads(response.text)
-            with open(f"{self.uuid}_{chunk}.json", "w", encoding="utf-8") as f:
+            with open(f"{self.created}_{self.uuid}_{chunk}.json", "w", encoding="utf-8") as f:
                 json.dump(response_json, f, ensure_ascii=False, indent=4)
             print("Download successful")
         return 0
@@ -46,7 +48,7 @@ class VulnExport:
     #     url = f"https://cloud.tenable.com/vulns/export/{vuln_export.uuid}/status"
     #     headers = {
     #         "accept": "application/json",
-    #         "X-ApiKeys": "accessKey=7becc28f63abced104af4af8c52f73cd21d3d36fd771172e03eaf4ceb241964f; secretKey=37f61b104ed8983d84f38e95d1470f07c620dfc4995263933fe2a43b49acf636;"
+    #         "X-ApiKeys": f"accessKey={os.environ["TENABLE_ACCESS_KEY"]}; secretKey={os.environ["TENABLE_SECRET_KEY"]};"
     #     }
     #     response = requests.get(url, headers=headers)
     #     response_json = json.loads(response.text)
