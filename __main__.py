98e2f14f8dda623865859115a1218a56ea6cb0d3
jcornall <james.cornall@stfc.ac.uk>
2024-06-25 16:57:51 +0100

    Implement logging and config

--- __main__.py
+++ __main__.py
@@ -1,50 +1,8 @@
-from util.vuln_export import *
+from vuln_export import *
 # from util.asset_export import *
-import logging
-import requests
-import os
-import json
-import logging
-import datetime
 
-current_time = datetime.current_time()
-logging.basicConfig(filename=f"{current_time.strftime("%Y%m%d_%H%M%S")}.log", filemode="w", format="%(asctime)s:%(name)s:%(levelname)s - %(message)s")
-
-def request_vuln_export():
-    url = "https://cloud.tenable.com/vulns/export"
-    payload = {
-        "num_assets": 50,
-        "include_unlicensed": True
-    }
-    headers = {
-        "accept": "application/json",
-        "content-type": "application/json",
-        "X-ApiKeys": f"accessKey={os.environ["TENABLE_ACCESS_KEY"]}; secretKey={os.environ["TENABLE_SECRET_KEY"]};"
-    }
-    response = requests.post(url, json=payload, headers=headers)
-    response_json = json.loads(response.text)
-    export_uuid = response_json["export_uuid"]
-    return 0, export_uuid
-
-def request_vuln_export_status(export_uuid):
-    url = f"https://cloud.tenable.com/vulns/export/{export_uuid}/status"
-    headers = {
-        "accept": "application/json",
-        "X-ApiKeys": f"accessKey={os.environ["TENABLE_ACCESS_KEY"]}; secretKey={os.environ["TENABLE_SECRET_KEY"]};"
-    }
-    response = requests.get(url, headers=headers)
-    response_json = json.loads(response.text)
-    vuln_export = VulnExport(response_json)
-    print(f"Export {vuln_export.uuid}:" +
-          f"\n - Export Status: {vuln_export.status}" +
-          f"\n - Total Chunks: {vuln_export.total_chunks}")
-    return vuln_export
-
-def check_data_folder():
-    print("Checking if /pmt-dashboard/data/ exists...")
-    if os.path.isdir("./data/"):
-        print("Directory exists.")
-    else:
-        print("Directory does not exist, creating directory...")
-        os.mkdir("data")
-        print("Directory created.")
+test_vuln_export = VulnExport()
+# test_uuid = test_vuln_export.request_vuln_export()
+test_uuid = "fa957f6e-6376-45d1-b106-e0f0d969e78f"
+test_vuln_export.request_vuln_export_status(test_uuid)
+test_vuln_export.download_all_vuln_chunks()
