481b32db39b2719c914c24213f249038f127ccd9
jcornall <james.cornall@stfc.ac.uk>
2024-06-26 14:20:43 +0100

    Modify __main__ and implement file system methods

--- __main__.py
+++ __main__.py
@@ -1,8 +1,15 @@
-from vuln_export import *
+from util.vuln_export import *
+from util.config import *
 # from util.asset_export import *
 
-test_vuln_export = VulnExport()
-# test_uuid = test_vuln_export.request_vuln_export()
-test_uuid = "fa957f6e-6376-45d1-b106-e0f0d969e78f"
-test_vuln_export.request_vuln_export_status(test_uuid)
-test_vuln_export.download_all_vuln_chunks()
+def main():
+    setup_logger()
+    setup_data()
+    vuln_export = VulnExport()
+    # vuln_export.request_vuln_export()
+    vuln_export.uuid = "95b21172-94f8-4800-b55b-801d5b9f3414"
+    vuln_export.request_vuln_export_status()
+    vuln_export.download_all_vuln_chunks()
+
+if __name__ == "__main__":
+    main()
