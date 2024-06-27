ab0a819da072b73a17d4ab071280671425883775
jcornall <james.cornall@stfc.ac.uk>
2024-06-27 10:33:06 +0100

    Save changes to __main__.py

--- __main__.py
+++ __main__.py
@@ -6,8 +6,7 @@ def main():
     setup_logger()
     setup_data()
     vuln_export = VulnExport()
-    # vuln_export.request_vuln_export()
-    vuln_export.uuid = "95b21172-94f8-4800-b55b-801d5b9f3414"
+    vuln_export.request_vuln_export()
     vuln_export.request_vuln_export_status()
     vuln_export.download_all_vuln_chunks()
 
