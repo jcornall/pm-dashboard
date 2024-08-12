49ac5654839efdd1fc444774ca660c66b9ceed67
jcornall <james.cornall@stfc.ac.uk>
2024-08-12 10:33:19 +0100

    Change args parameter to regular parameter

--- src/util/load/database_loader.py
+++ src/util/load/database_loader.py
@@ -15,7 +15,7 @@ class DatabaseLoader():
         self.table = table
         self.cursor = self.set_cursor
 
-    def connect_to_database(self, *connection_parameters):
+    def connect_to_database(self, connection_parameters):
         logging.info(f"Connecting to {connection_parameters["host"]} as {connection_parameters["user"]}...")
         try:
             conn = mariadb.connect(connection_parameters)
