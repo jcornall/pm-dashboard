6f332fc4bfff08fdd32d80256f9507f3a1ab790c
jcornall <james.cornall@stfc.ac.uk>
2024-08-14 14:10:38 +0100

    Modify __main__.sh to include OS-specific commands

--- __main__.sh
+++ __main__.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-
 # Shell script to execute the __main__.py module with the virtual environment interpreter
 source ~/.bash_profile
-
 PYSCRIPT_NAME="__main__.py"
-
 CWD="$(dirname -- "$0")"
 PATH="$CWD/.venv/Scripts:$PATH"
-
+if [[ "$OSTYPE" == "linux-gnu"* ]];
+then
+    PATH="$CWD/.venv/bin:$PATH"
+elif [[ "$OSTYPE" == "msys" ]];
+then
+    PATH="$CWD/.venv/Scripts:$PATH"
+fi
 "$CWD/$PYSCRIPT_NAME" "$@"
--- install.sh
+++ install.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-
 # Shell script to setup and activate a Python virtual environment
 python3.12 -m pip install virtualenv
 python3.12 -m virtualenv .venv
-
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     source .venv/bin/activate
     pip install -r requirements.txt
