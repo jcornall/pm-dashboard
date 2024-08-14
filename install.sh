24c9a4b6b2d2dc8e1e95e1b29fdc1715d8a5fa76
jcornall <james.cornall@stfc.ac.uk>
2024-08-14 11:36:56 +0100

    Add OS-specific commands to install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,11 @@
 # Shell script to setup and activate a Python virtual environment
 python3.12 -m pip install virtualenv
 python3.12 -m virtualenv .venv
-source .\.venv\Scripts\activate
-pip install -r requirements.txt
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    source .venv/bin/activate
+    pip install -r requirements.txt
+else [[ "$OSTYPE" == "msys" ]]; then
+    source .\\venv\\Scripts\\activate
+    pip install -r requirements.txt
+fi
