fedca3cac219d0deba4f4de309284a8eb5a224b6
jcornall <pam62425@stfc.ac.uk>
2024-08-20 10:02:59 +0100

    Update test.py with changes made to __main__.py

--- test.py
+++ test.py
@@ -15,7 +15,7 @@ from src.config.logger_config import *
 
 
 def export_vuln():
-    #  Sequence API calls to Tenable service in order to download vulnerability data
+    """Sequence API calls to Tenable service and return downloaded vulnerability data."""
     vuln_export = VulnExport()
     vuln_export.post_vuln_export()
     vuln_export.get_vuln_export_status()
@@ -23,29 +23,34 @@ def export_vuln():
     return 0
 
 def export_asset():
-    #  Sequence API calls to Tenable service in order to download asset data
+    """Sequence API calls to Tenable service and return downloaded asset data."""
     asset_export = AssetExport()
     asset_export.post_asset_export()
     asset_export.get_asset_export_status()
-    asset_export.get_asset_export_jobs()  #  Required due to asset export status metadata differing from vulnerability export metadata
+    asset_export.get_asset_export_jobs()  # Required due to asset export status metadata differing from vulnerability export metadata
     asset_export.download_all_asset_chunks()
     return 0
 
 def process_data(file_path):
+    """Sequence DataProcessor calls to DataProcessor."""
     data_processor = DataProcessor(file_path)
     data_processor.transform_data(data_processor.dir_path, data_processor.header_file, data_processor.export_type)
     # configure_data_processor(data_processor)
-    # data_processor.merge_data()
     return 0
 
 def configure_data_processor(data_processor):
+    """Configure the DataProcessor by generating new YAML files containing all JSON data headers."""
     generate_header_yaml(data_processor)
 
-def load_data(table, create_statement, load_statement):
+def load_data(table):
+    """Load the data into a MariaDB database."""
     database_loader = DatabaseLoader(table, CONN_PARAMS)
-    database_loader.drop_table(database_loader.cursor, "tenable", table)
-    database_loader.create_table(database_loader.cursor, create_statement)
-    database_loader.load_csv(database_loader.cursor, load_statement)
+    # database_loader.drop_table(database_loader.cursor, "tenable", table)
+    database_loader.create_table(database_loader.cursor, "create_vuln_export_table.sql")
+    database_loader.create_table(database_loader.cursor, "create_vuln_timeseries_table.sql")
+    database_loader.load_csv(database_loader.cursor, "load_vuln_csv.sql")
+    database_loader.insert_into_table(database_loader.cursor, "insert_into_vuln_csv.sql")
+    database_loader.delete_from_table(database_loader.cursor, "delete_from_vuln_export_table.sql")
     database_loader.conn.commit()
     database_loader.cursor.close()
     database_loader.close_connection()
@@ -79,15 +84,9 @@ logging.info("File structure setup successful.")
 # process_data(ASSET_EXPORT_DIR)
 # logging.info("Data transformation successful.")
 
-def load_data(table):
-    """Load the data into a MariaDB database."""
-    database_loader = DatabaseLoader(table, CONN_PARAMS)
-    # database_loader.drop_table(database_loader.cursor, "tenable", table)
-    database_loader.create_table(database_loader.cursor, "create_vuln_export_table.sql")
-    database_loader.create_table(database_loader.cursor, "create_vuln_timeseries_table.sql")
-    database_loader.load_csv(database_loader.cursor, "load_vuln_csv.sql")
-    database_loader.insert_into_table(database_loader.cursor, "insert_into_vuln_csv.sql")
-    database_loader.delete_from_table(database_loader.cursor, "delete_from_vuln_export_table.sql")
-    database_loader.conn.commit()
-    database_loader.cursor.close()
-    database_loader.close_connection()
+# Load
+logging.info("Starting data loading...")
+load_data("vulnerabilities")
+
+logging.info("Program execution successful, exiting program.")
+sys.exit(0)
