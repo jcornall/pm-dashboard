c85677bec3905f13e83bca368b7685e5cc1c2a9a
jcornall <pam62425@stfc.ac.uk>
2024-08-20 12:27:04 +0100

    Modify multiple SQL statements

--- src/config/resource/sql/insert_into_vuln_table.sql
+++ src/config/resource/sql/insert_into_vuln_table.sql
@@ -1,2 +1,11 @@
 INSERT INTO vulnerabilities_timeseries
-SELECT DISTINCT FROM vulnerabilities_export;
+SELECT * FROM vulnerabilities_export
+WHERE NOT EXISTS (
+    SELECT * FROM vulnerabilities_timeseries
+    WHERE (
+        vulnerabilities_export.created_on = vulnerabilities_timeseries.created_on AND
+        vulnerabilities_export.asset_fqdn = vulnerabilities_timeseries.asset_fqdn AND
+        vulnerabilities_export.asset_ipv4 = vulnerabilities_timeseries.asset_ipv4 AND
+        vulnerabilities_export.plugin_name = vulnerabilities_timeseries.plugin_name
+    )
+);
