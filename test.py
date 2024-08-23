f4c99a380d836aa04c9a9f1c825aa86998146dc4
jcornall <pam62425@stfc.ac.uk>
2024-08-23 15:24:31 +0100

    Update load_data() method to accommodate transformation and loading of asset data

--- test.py
+++ test.py
@@ -42,15 +42,15 @@ def configure_data_processor(data_processor):
     """Configure the DataProcessor by generating new YAML files containing all JSON data headers."""
     generate_header_yaml(data_processor)
 
-def load_data(table):
+def load_data(data_type, sql_file_path):
     """Load the data into a MariaDB database."""
-    database_loader = DatabaseLoader(table, CONN_PARAMS)
+    database_loader = DatabaseLoader(data_type, CONN_PARAMS)
     # database_loader.drop_table(database_loader.cursor, "tenable", table)
-    database_loader.create_table(database_loader.cursor, "create_vuln_export_table.sql")
-    database_loader.create_table(database_loader.cursor, "create_vuln_timeseries_table.sql")
-    database_loader.load_csv(database_loader.cursor, "load_vuln_csv.sql")
-    database_loader.insert_into_table(database_loader.cursor, "insert_into_vuln_table.sql")
-    database_loader.delete_from_table(database_loader.cursor, "delete_from_vuln_export_table.sql")
+    database_loader.create_table(database_loader.cursor, sql_file_path, "create_export_table.sql")
+    database_loader.create_table(database_loader.cursor, sql_file_path, "create_timeseries_table.sql")
+    database_loader.load_csv(database_loader.cursor, sql_file_path, "load_csv.sql")
+    database_loader.insert_into_table(database_loader.cursor, sql_file_path, "insert_into_timeseries_table.sql")
+    database_loader.delete_from_table(database_loader.cursor, sql_file_path, "delete_from_export_table.sql")
     database_loader.conn.commit()
     database_loader.cursor.close()
     database_loader.close_connection()
