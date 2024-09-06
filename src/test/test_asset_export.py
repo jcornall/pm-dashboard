878f4ba3043b9ed19ab70273d4ee1e09a9a69d24
jcornall <pam62425@stfc.ac.uk>
2024-09-06 17:00:07 +0100

    Implement AssetExport() unit testing

---
+++ src/test/test_asset_export.py
@@ -0,0 +1,26 @@
+import unittest
+from unittest.mock import MagicMock
+import requests as re
+from src.util.extract.api_export import *
+
+class TestMain(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_main(self):
+
+
+if __name__ == "__main__":
+    unittest.main()
