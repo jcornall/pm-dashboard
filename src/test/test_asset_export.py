96d4c127fb069bec6c92a3dae83af5cc97d214e3
jcornall <pam62425@stfc.ac.uk>
2024-09-23 16:52:12 +0100

    Add test_set_values() test method

--- src/test/test_asset_export.py
+++ src/test/test_asset_export.py
@@ -1,26 +1,35 @@
-import unittest
-from unittest.mock import MagicMock
-import requests as re
-from src.util.extract.api_export import *
+import pytest
+import os
+from src.config.constants import *
+from src.util.extract.asset_export import *
 
-class TestMain(unittest.TestCase):
+def test_set_values(mocker):
 
-    @classmethod
-    def setUpClass(cls):
-        pass
+    mock_obj = mocker.patch("src.util.extract.asset_export.AssetExport")
 
-    @classmethod
-    def tearDownClass(cls):
-        pass
+    test_response_json = {
+        "uuid": "3faa6edf-5a06-496f-8898-2f60b32c14bf",
+        "status": "FINISHED",
+        "total_chunks": 1,
+        "filters": "{\"tag.<category>\":\"tag.last_scan: test sf01\"}",
+        "finished_chunks": 1,
+        "num_assets_per_chunk": 100,
+        "created": 1616117580482
+    }
 
-    def setUp(self):
-        pass
+    mock_obj.set_values(test_response_json)
+    mock_obj.set_values.assert_called_once()
 
-    def tearDown(self):
-        pass
+    assert mock_obj.uuid == test_response_json["uuid"]
+    assert mock_obj.total_chunks == test_response_json["total_chunks"]
+    assert mock_obj.filters == test_response_json["filters"]
+    assert mock_obj.finished_chunks == test_response_json["finished_chunks"]
+    assert mock_obj.num_assets_per_chunk == test_response_json["num_assets_per_chunk"]
+    assert mock_obj.created == test_response_json["created"]
 
-    def test_main(self):
-
-
-if __name__ == "__main__":
-    unittest.main()
+    assert isinstance(mock_obj.uuid, str) == True
+    assert isinstance(mock_obj.total_chunks, int) == True
+    assert isinstance(mock_obj.filters, str) == True
+    assert isinstance(mock_obj.finished_chunks, int) == True
+    assert isinstance(mock_obj.num_assets_per_chunk, int) == True
+    assert isinstance(mock_obj.created, int) == True
